5c2b38178efc9fc2bba26b1bd780b87e094c17ef
Preshen4 <preshen4@gmail.com>
2023-03-19 20:40:06 +0200

    Initial commit

---
+++ app/src/main/java/com/example/ontime/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.ontime
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        // Removes the title bar
+        supportActionBar?.hide()
+    }
+}
