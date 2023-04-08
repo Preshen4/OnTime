b5048c7334aa7c79e67ef2d3eb14e58f51756585
Preshen4 <preshen4@gmail.com>
2023-04-08 22:34:59 +0200

    Responsive UI

--- app/src/main/java/com/example/ontime/fragments/ui/addProject/AddProjectFragment.kt
+++ app/src/main/java/com/example/ontime/fragments/ui/addProject/AddProjectFragment.kt
@@ -1,22 +1,22 @@
 package com.example.ontime.fragments.ui.addProject
 
-import android.app.Dialog
-import android.graphics.Color
-import android.graphics.drawable.ColorDrawable
+import android.app.DatePickerDialog
+import android.content.DialogInterface
+import android.icu.util.Calendar
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.WindowManager
-import android.widget.Button
-import android.widget.DatePicker
-import android.widget.Toast
 import com.example.ontime.R
+import com.example.ontime.databinding.FragmentAddProjectBinding
+
 
 class AddProjectFragment : Fragment() {
+    private var _binding: FragmentAddProjectBinding? = null
 
+    private val binding get() = _binding!!
     companion object {
         fun newInstance() = AddProjectFragment()
     }
@@ -27,7 +27,29 @@ class AddProjectFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_add_project, container, false)
+        _binding = FragmentAddProjectBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        binding.btnStartDate.setOnClickListener {
+            val calendar = Calendar.getInstance()
+            val year = calendar.get(Calendar.YEAR)
+            val month = calendar.get(Calendar.MONTH)
+            val dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH)
+
+            val datePickerDialog = DatePickerDialog(requireContext(), null, year, month, dayOfMonth)
+            datePickerDialog.setButton(DialogInterface.BUTTON_POSITIVE, "OK") { dialog, which ->
+                // Handle date selection here
+            }
+            datePickerDialog.setButton(DialogInterface.BUTTON_NEGATIVE, "Cancel") { dialog, which ->
+                // Handle cancel button click here
+            }
+
+            val customView = layoutInflater.inflate(R.layout.custom_date_picker_dialog, null)
+            datePickerDialog.setView(customView)
+            datePickerDialog.show()
+        }
+
+        return root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
